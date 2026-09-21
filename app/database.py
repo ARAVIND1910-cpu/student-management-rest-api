89a7f4940c384835c286a55ce63d62f4291cb84f
ARAVIND1910-cpu <arumugam.mm1973@gmail.com>
2026-09-21 11:15:22 +0530

    Make database URL configurable

--- app/database.py
+++ app/database.py
@@ -1,12 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./students.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./students.db")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
